ea0fcb93bbb5807abd250ede5e059e989a61ffb7
Irina M <i-a-medvedeva@yandex.ru>
2021-11-29 23:37:09 +0300

    Added task 014

---
+++ task014/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+//Console.WriteLine(num);
+//int N=num/100;
+while (num >= 1000)
+{
+    num = num/10;   
+}
+    if (num<100)
+{
+   Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    Console.WriteLine (num%10);
+}
