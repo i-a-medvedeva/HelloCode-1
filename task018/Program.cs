4f3bfdab29b783a3f2b7229175bd8fa83463323c
Irina M <i-a-medvedeva@yandex.ru>
2021-11-29 23:38:54 +0300

    Added task 018

---
+++ task018/Program.cs
@@ -0,0 +1,18 @@
+﻿//Console.WriteLine("Введите true или false ");
+//bool numX = bool.Parse(Console.ReadLine());
+//Console.WriteLine(numX);
+Console.WriteLine("Введите первое число true или false :");
+bool number1 = bool.Parse(Console.ReadLine());
+Console.WriteLine(number1);
+Console.WriteLine("Введите второе число false или true:");
+bool number2 = bool.Parse(Console.ReadLine());
+Console.WriteLine(number2);
+if (!(number1 | number2 ) == (!number1 & !number2))
+{
+   Console.WriteLine($"Закон работает!");
+    
+}
+else
+{
+    Console.WriteLine("Закон не работает!");
+}
