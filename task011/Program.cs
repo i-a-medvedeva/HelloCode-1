ed76290bf6c11fc80b8969dea7293c001fef6c6e
Irina M <i-a-medvedeva@yandex.ru>
2021-12-09 19:06:34 +0300

    Added task011

---
+++ task011/Program.cs
@@ -0,0 +1,16 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+int X = num / 10;
+int Y = num % 10;
+if (X>Y)
+{
+    Console.WriteLine(X);
+}
+else
+{
+    Console.WriteLine(Y);
+}
+
