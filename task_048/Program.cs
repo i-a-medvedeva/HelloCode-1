b49700d41f639ad5eaf93dd8948c5bfe1ebf5f8d
Irina M <i-a-medvedeva@yandex.ru>
2021-12-09 17:50:04 +0300

    Added task_048 two-dimensional array

---
+++ task_048/Program.cs
@@ -0,0 +1,26 @@
+﻿//48. Показать двумерный массив размером m×n заполненный целыми числами
+int m = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(m);
+int n = new Random().Next(1, 10);
+Console.WriteLine(n);
+int[,] Array = new int [m, n];
+//int[,] Array = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+
+
+void printDoubleArray(int[,] Array)
+{
+    for (int row = 0; row < Array.GetLength(0); row++)
+    {
+        for (int column = 0; column < Array.GetLength(1); column++)
+        {
+            Array[row,column] = new Random().Next(1, 10);
+            Console.Write(Array[row, column] + " ");
+        }
+        Console.WriteLine(); // отступ на новую строку
+    }
+}
+
+printDoubleArray(Array);
+// Console.WriteLine(); //отступ на новую строку
+// printDoubleArray(Array);
+
