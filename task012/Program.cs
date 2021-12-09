1b1899b4d492bd81df4145b959a279d87ad02797
Irina M <i-a-medvedeva@yandex.ru>
2021-12-09 19:14:49 +0300

    Added task012

---
+++ task012/Program.cs
@@ -0,0 +1,13 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+
+Console.Write ("Введите трехзначное число ...");
+int num = int.Parse(Console.ReadLine());
+
+int X = num / 10;
+int Y = num % 10;
+int A = num/100;
+
+    Console.Write(A);
+    Console.Write(Y);
+
+
