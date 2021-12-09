6f23e94250ac55f7574c9dec6887917bc2deac9d
Irina M <i-a-medvedeva@yandex.ru>
2021-12-09 19:33:22 +0300

    Added task009

---
+++ task009/Program.cs
@@ -0,0 +1,6 @@
+﻿//9. Показать последнюю цифру трёхзначного числа
+
+Console.Write ("Введите трехзначное число ...");
+int num = int.Parse(Console.ReadLine());
+int N = num%10;
+Console.WriteLine(N);
--- task010/Program.cs
+++ task010/Program.cs
@@ -5,3 +5,5 @@ int num = int.Parse(Console.ReadLine());
 int N = num/10;
 int X=N%10;
 Console.WriteLine(X);
+
+
