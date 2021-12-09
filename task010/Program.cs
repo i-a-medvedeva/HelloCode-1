1703ddc82dcd8bf71a256ecfae5b3cf433ea7347
Irina M <i-a-medvedeva@yandex.ru>
2021-12-09 18:58:13 +0300

    Add task010

---
+++ task010/Program.cs
@@ -0,0 +1,7 @@
+﻿//10. Показать вторую цифру трёхзначного числа
+
+Console.Write ("Введите трехзначное число ...");
+int num = int.Parse(Console.ReadLine());
+int N = num/10;
+int X=N%10;
+Console.WriteLine(X);
